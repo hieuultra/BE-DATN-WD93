71e63a1d41f3ae1f37c2c2f4f3dc39d4b83741ca
Thangng <118162381+thangpo@users.noreply.github.com>
2024-10-19 22:08:46 +0700

    thangnh_task10_QL_specialties_doctor

--- DATN_WD-93/routes/web.php
+++ DATN_WD-93/routes/web.php
@@ -5,7 +5,9 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\DoctorController;
 use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\SpecialtyController;
 //client
 use App\Http\Controllers\Client\AuthController;
 use App\Http\Controllers\Client\HomeController;
@@ -75,6 +77,32 @@ Route::middleware(['auth', 'auth.admin'])->prefix('admin')
                 Route::post('/productUpdate', [ProductController::class, 'productUpdate'])->name('productUpdate');
                 Route::delete('/productDestroy/{id}', [ProductController::class, 'productDestroy'])->name('productDestroy');
             });
+        //specialties
+        Route::prefix('specialties')
+            ->as('specialties.')
+            ->group(function () {
+                Route::get('/specialtyList', [SpecialtyController::class, 'index'])->name('specialtyList');
+                Route::post('/specialtyAdd', [SpecialtyController::class, 'store']);
+                Route::put('/specialtyUpdate/{id}', [SpecialtyController::class, 'update']);
+                Route::delete('/specialtyDestroy/{id}', [SpecialtyController::class, 'destroy']);
+            });
+        //doctors
+        Route::prefix('doctors')
+            ->as('doctors.')
+            ->group(function () {
+                Route::get('/doctorsList', [DoctorController::class, 'index'])->name('doctors.index');
+                Route::post('/doctorsAdd', [DoctorController::class, 'store'])->name('doctors.store');
+                Route::get('/doctorsEdit/{id}', [DoctorController::class, 'show'])->name('doctors.show');
+                Route::get('/doctorsDetails/{id}', [DoctorController::class, 'showDetails']);
+                Route::put('/doctorsUpdate/{id}', [DoctorController::class, 'update'])->name('doctors.update');
+                Route::delete('/doctorsDestroy/{id}', [DoctorController::class, 'destroy'])->name('doctors.destroy');
+
+                Route::get('/doctor/schedule/{doctorId}', [DoctorController::class, 'showSchedule'])->name('doctor.schedule');
+                Route::post('/doctor/scheduleAdd', [DoctorController::class, 'scheduleAdd']);
+                Route::get('/doctor/scheduleEdit/{id}', [DoctorController::class, 'scheduleEdit']);
+                Route::put('/doctor/scheduleUpdate/{id}', [DoctorController::class, 'scheduleUpdate']);
+                Route::delete('/doctor/scheduleDestroy/{id}', [DoctorController::class, 'scheduleDestroy']);
+            });
         //order
         // Route::prefix('bills')
         //     ->as('bills.')
@@ -96,3 +124,11 @@ Route::middleware(['auth', 'auth.admin'])->prefix('admin')
         //         Route::delete('/accDestroy/{id}', [AccountController::class, 'accDestroy'])->name('accDestroy');
         //     });
     });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
